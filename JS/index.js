edb9d0533919a72cfd9e3bf713debd1325e32a01
Josue <jmcure2@gmail.com>
2023-08-26 21:49:25 -0500

    commit

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,18 @@
 
-/* 
-   const card = document.getElementById('card1');
-    card.addEventListener('click', () =>{
 
-    document.getElementById('card2').style.width="80px";
-    document.getElementById('card1').style.width="700px";
+const cards = document.querySelectorAll('div.cards');
+
+cards.forEach(carta => {
+   carta.addEventListener('click',() => {
+      removeCard()
+   carta.classList.add('active'); 
    })
- */
-
-   const cards = document.querySelectorAll('div.cards');
-
-cards.forEach(card => {
-  card.addEventListener("click", cambio);
-});
-
-function cambio(event) {
-  event.target.classList.toggle('card1');
-  event.target.classList.toggle('cards');
-}
-
- 
+   
+}) //
 
 
-
-
+function removeCard() {
+   cards.forEach(cards => {
+   cards.classList.remove('active');  
+   })
+};
